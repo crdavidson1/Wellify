49810d9aaf2f86c392639e38c217df5a2375d933
xiamonder <acldiamond@gmail.com>
2024-03-22 15:44:38 +0000

    updated camfeed for ts

--- src/renderer/src/components/CamFeed.tsx
+++ src/renderer/src/components/CamFeed.tsx
@@ -1,21 +1,21 @@
-import Webcam from "react-webcam";
-
-export default function CamFeed() {
+import Webcam from 'react-webcam'
+import React from 'react'
+const CamFeed: React.FC = () => {
   return (
     <Webcam
       style={{
-        position: "absolute",
-        marginLeft: "auto",
-        marginRight: "auto",
+        position: 'absolute',
+        marginLeft: 'auto',
+        marginRight: 'auto',
         left: 0,
         right: 0,
-        textAlign: "center",
+        textAlign: 'center',
         zIndex: 9,
         width: 1200,
-        height: 800,
+        height: 800
       }}
     />
   )
-
-
 }
+
+export default CamFeed
