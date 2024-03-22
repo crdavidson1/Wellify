11a26e964b5bab8a1a78191d9e13497184bfd220
xiamonder <acldiamond@gmail.com>
2024-03-22 14:43:39 +0000

    eslint and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,18 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
     '@electron-toolkit/eslint-config-ts/recommended',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 }
