61e49d61b4d742a42d8b5bc2409a9ce21abd9ab7
Christopher Li <mr.robbob@hotmail.co.uk>
2024-03-26 17:21:31 +0000

    local sql connection works, open connection is left hanging

--- src/backend/database/connection.ts
+++ src/backend/database/connection.ts
@@ -1,18 +1,25 @@
-import { createPool, Pool } from 'mysql2/promise'
-// import { dotenv } from 'dotenv'
-
-// dotenv.config()
+const mysql = require('mysql2/promise.js')
 
 const databaseName: string = process.env.NODE_ENV === 'test' ? 'wellify_test' : 'wellify_db'
 
-const pool: Pool = createPool({
+const fn = async (): Promise<void> => {
+  const connection = await mysql.createConnection ({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USER || 'robbob',
+  password: process.env.DB_PASSWORD || 'robbob',
   database: databaseName,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-})
+  })
+  
+  connection.query(`
+    select * from example;
+    `)
+  .then((res) => {
+    console.log(res);
+  
+  })
+}
 
-export default pool
+fn()
