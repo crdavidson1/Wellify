ccb8202ec9efac8ad53de0e432d83678635a911f
crdavidson1 <callum.r.davidson@hotmail.com>
2024-03-27 12:20:36 +0000

    adds chart.js functionality

---
+++ src/renderer/src/components/Charts/ApplicationChart.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { useState } from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
+import {faker} from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const ApplicationChart: React.FC = () => {
+        const options3 = {
+    plugins: {
+        title: {
+        display: true,
+        text: 'Emotions by Application',
+        },
+    },
+    responsive: true,
+    scales: {
+        x: {
+        stacked: true,
+        },
+        y: {
+        stacked: true,
+        },
+    },
+    };
+
+    const labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    const data3 = {
+    labels: ['VSCode', 'Chrome', 'Spotify', 'Slack', 'Terminal'],
+    datasets: [
+        {
+        label: 'Happy',
+        data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+        backgroundColor: 'rgb(255, 99, 132)',
+        },
+        {
+        label: 'Sad',
+        data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+        backgroundColor: 'rgb(75, 192, 192)',
+        },
+        {
+        label: 'Angry',
+        data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+        backgroundColor: 'rgb(53, 162, 235)',
+        },
+        {
+        label: 'Fear',
+        data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+        backgroundColor: 'rgb(255, 206, 86',
+        },
+        {
+        label: 'Disgust',
+        data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+        backgroundColor: 'rgb(255, 159, 64)',
+        },
+        {
+        label: 'Neutral',
+        data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
+        backgroundColor: 'rgb(153, 102, 255)',
+        },
+    ],
+    };
+
+    return (
+    <div style={{height:'400px'}}>
+        <Bar options={options3} data={data3} />;
+    </div>
+  )
+}
+
+export default ApplicationChart
--- src/renderer/src/components/Statistics.tsx
+++ src/renderer/src/components/Statistics.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useState } from 'react'
+import SlouchChart from './Charts/SlouchChart';
+import EmotionChart from './Charts/EmotionChart';
+import ApplicationChart from './Charts/ApplicationChart';
 
 const Statistics: React.FC = () => {
-    return <h1>Test Statistics</h1>
+    const [chartType, setChartType] = useState()
+
+    function handleChange(event) {
+        setChartType(event.target.value)
+    }
+    let chart = <SlouchChart/>
+    if (chartType === '1') {
+        chart = <SlouchChart/>
+    }
+    if (chartType === '2') {
+        chart = <EmotionChart/>
+    }
+    if (chartType === '3') {
+        chart = <ApplicationChart/>
+    }
+
+  return (
+    <div style={{height:'400px'}}>
+        <label>Select a chart: </label>
+            <select value={chartType} onChange={handleChange} name='Select Chart' id='model-performance'>
+                <option value={'1'}>Slouches This Week</option>
+                <option value='2'>Emotions This Week</option>
+                <option value='3'>Emotions By Application</option>
+            </select>
+        {chart}
+    </div>
+  )
 }
 
 export default Statistics
