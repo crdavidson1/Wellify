6c728394834e724ff67025775ba6d2a5fb0ff784
crdavidson1 <callum.r.davidson@hotmail.com>
2024-03-28 15:52:33 +0000

    adds usecontext to all settings

--- src/renderer/src/contexts/User.tsx
+++ src/renderer/src/contexts/User.tsx
@@ -3,16 +3,26 @@ import { createContext, useState } from 'react'
 export const UserContext = createContext<{
   modelComplexity: 0 | 1 | 2 | undefined
   setModelComplexity: (newValue) => void
+  camera: string
+  setCamera: (newValue) => void
+  postureStrictness: string
+  setPostureStrictness: (newValue) => void
 }>({
   modelComplexity: 1,
-  setModelComplexity: () => undefined
+  setModelComplexity: () => undefined,
+  camera: '',
+  setCamera: () => undefined,
+  postureStrictness: '',
+  setPostureStrictness: () => undefined
 })
 
 export const UserProvider: React.FC<any> = ({ children }) => {
   const saved = JSON.parse(localStorage.getItem('currentUser') || '1')
   const [modelComplexity, setModelComplexity] = useState(saved)
+  const [camera, setCamera] = useState('')
+  const [postureStrictness, setPostureStrictness] = useState('')
   return (
-    <UserContext.Provider value={{ modelComplexity, setModelComplexity }}>
+    <UserContext.Provider value={{ modelComplexity, setModelComplexity, camera, setCamera, postureStrictness, setPostureStrictness }}>
       {children}
     </UserContext.Provider>
   )
