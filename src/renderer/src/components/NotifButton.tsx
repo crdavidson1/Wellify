53089f515f954c8dd980d15eba6b7ae144bdbea8
Christopher Li <mr.robbob@hotmail.co.uk>
2024-03-28 11:30:10 +0000

    sql changes, emotion changes, installed express

--- src/renderer/src/components/NotifButton.tsx
+++ src/renderer/src/components/NotifButton.tsx
@@ -1,21 +1,21 @@
 
 const NotifButton: React.FC = () => {
-    async function handleClick(): Promise<void> {
-        const res = await window.wellifyAPI.check()
-        console.log(res);
-        
-        Notification.requestPermission().then(() => {
-            const NOTIFICATION_TITLE = 'Title'
-            const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
+  async function handleClick(): Promise<void> {
+    const res = await window.wellifyAPI.check()
+    console.log(res);
 
-            new window.Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
-        })
-    }
-    return (
-        <button id='trigger' onClick={handleClick}>
-            Trigger Notification
-        </button>
-    )
+    Notification.requestPermission().then(() => {
+      const NOTIFICATION_TITLE = 'Title'
+      const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
+
+      new window.Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
+    })
+  }
+  return (
+    <button id='trigger' onClick={handleClick}>
+      Trigger Notification
+    </button>
+  )
 }
 
 export default NotifButton
