2859852ec705ce93fd9a494180d3838f8098fc43
crdavidson1 <callum.r.davidson@hotmail.com>
2024-03-28 10:13:18 +0000

    adds morphcast to same video element

--- src/renderer/src/components/EmotionTracking.tsx
+++ src/renderer/src/components/EmotionTracking.tsx
@@ -8,13 +8,15 @@ import FaceTrackerComponent from './Emotion/FaceTracker';
 import MoodComponent from './Emotion/Mood';
 import EmotionBarsComponent from './Emotion/EmotionBars';
 
-const EmotionTracking: React.FC = () => {
+const EmotionTracking: React.FC<any> = ({webcamRef}) => {
  const mphToolsState = useExternalScript("https://sdk.morphcast.com/mphtools/v1.0/mphtools.js");
   const aiSdkState = useExternalScript("https://ai-sdk.morphcast.com/v1.16/ai-sdk.js");
   const videoEl = useRef(undefined)
 
   useEffect(() => {
-    videoEl.current = document.getElementById("videoEl");
+    videoEl.current = document.getElementById("webcamRef");
+    console.log(document.getElementById('webcamRef'))
+    console.log(webcamRef)
     async function getAiSdk (){
       if(aiSdkState === "ready" && mphToolsState === "ready"){
         const { source, start } = await getAiSdkControls();
@@ -32,13 +34,9 @@ const EmotionTracking: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{display:"flex", flexDirection: "column", alignItems:"center"}}>
-          <div style={{width:"640px", height: "480px", position:"relative"}}>
-            <video id="videoEl"></video>
+        <div style={{display:"flex", flexDirection: "column", alignItems:"center"}}>  
             <FaceTrackerComponent videoEl={videoEl}></FaceTrackerComponent>
-          </div>
           <DominantEmotion></DominantEmotion>
-          <hr className="solid" style={{width:"100%"}}></hr>
         </div>
       </header>
     </div>
