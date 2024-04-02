c9a40ea76a28f13b8974a0b44188b6f19299c346
crdavidson1 <callum.r.davidson@hotmail.com>
2024-04-02 16:31:39 +0100

    disables background throttling

--- src/main/index.ts
+++ src/main/index.ts
@@ -31,7 +31,8 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: false,
+      backgroundThrottling: false
     }
   })
 
